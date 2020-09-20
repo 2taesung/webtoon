a7e1f1a50972cfcce5f17a87edd33a77eefa17ef
2taesung <66891085+2taesung@users.noreply.github.com>
2020-09-20 16:29:16 +0900

    superuser and ranking data structure

--- wr_project/admin_app/admin.py
+++ wr_project/admin_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from admin_app.models import action, romance, fantasy
 
 # Register your models here.
+
+
+admin.site.register(action)
+admin.site.register(romance)
+admin.site.register(fantasy)
--- wr_project/admin_app/models.py
+++ wr_project/admin_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class action(models.Model):
+    before_ranking = models.IntegerField(default=10000)
+    after_ranking = models.IntegerField(default=10000)
+
+    # def __str__(self):
+    #     return self.choice_text
+
+class romance(models.Model):
+    before_ranking = models.IntegerField(default=10000)
+    after_ranking = models.IntegerField(default=10000)
+
+class fantasy(models.Model):
+    before_ranking = models.IntegerField(default=10000)
+    after_ranking = models.IntegerField(default=10000)
