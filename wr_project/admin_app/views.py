8558701672e416e86431a4afda3d87d005f59b3a
2taesung <66891085+2taesung@users.noreply.github.com>
2020-09-20 18:27:57 +0900

    try views code delete

--- wr_project/admin_app/views.py
+++ wr_project/admin_app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-class1_students = ['이태성', '김혜란', '송두기', '이윤환']
 
 # Create your views here.
 def home(request):
-    name = '이태성'
-    return render(request, 'home.html', {'username':name})
+    return render(request, 'home.html')
 
 def login(request):
     return HttpResponse("login")
@@ -14,13 +12,5 @@ def login(request):
 def signout(request):
     return HttpResponse("Signout")
 
-def result(request):
-    # print('🤞', request.POST['username'])
-    username = request.POST['username']
-
-    if username in class1_students:
-        print('🐱‍🚀 1반의 학생이예요')
-    else :
-        print('🤷‍♀️ 1반의 학생이 아닙니다')
-
-    return render(request, 'result.html')
+def signup(request):
+    return HttpResponse("Signup")
--- wr_project/wr_project/urls.py
+++ wr_project/wr_project/urls.py
@@ -20,7 +20,7 @@ from admin_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
-    path('result/', views.result, name="result"),
+    path('bulletinboard/', views.result, name="bulletinboard"),
 ]
